44f854ed483f32a677f8b64aaf5911f23dfe81ec
Likeve <q1b2kb@qq.com>
2026-06-08 10:17:51 +0800

    chore: rename font files to Full name from TTF metadata, cleanup unreferenced files
    - Rename 452 TTF files to their embedded Full name (nameID=4)
    - Update fontPath in fonts.json to match new filenames
    - Remove 448 unreferenced TTF variants from public/fonts (moved to backup)
    - Fix upload-fonts.sh: add --remote and CLOUDFLARE_ACCOUNT_ID
    - Add wrangler.toml for Cloudflare R2 management

--- scripts/upload-fonts.sh
+++ scripts/upload-fonts.sh
@@ -3,6 +3,7 @@ set -e
 
 BUCKET="chinese-fonts"
 FONTS_DIR=$(cd "$(dirname "$0")/../public/fonts" && pwd -P)
+ACCOUNT_ID="${CLOUDFLARE_ACCOUNT_ID:-142c22318d084bc4a2f24b06ca44868b}"
 
 count=0
 total=$(ls "$FONTS_DIR" | wc -l | tr -d ' ')
@@ -11,7 +12,7 @@ for file in "$FONTS_DIR"/*.ttf; do
   count=$((count + 1))
   filename=$(basename "$file")
   echo "[$count/$total] Uploading: $filename"
-  wrangler r2 object put "$BUCKET/fonts/$filename" --file "$file" --remote
+  CLOUDFLARE_ACCOUNT_ID="$ACCOUNT_ID" npx wrangler r2 object put "$BUCKET/fonts/$filename" --file "$file" --remote
 done
 
 echo "Done! Uploaded $count font files to R2 bucket '$BUCKET'"
